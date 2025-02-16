1bb3b7bbb45e141fa462694113db1c6b884d607b
Hemjotkaur <hemjotkaur786@gmail.com>
2025-02-16 14:19:27 +0530

    made changes

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -42,7 +42,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: './Hemjot-resume.pdf',
+            href: './hemjotkaur-resume.pdf',
             style:'rounded-br-md',
             download:true,
         },
